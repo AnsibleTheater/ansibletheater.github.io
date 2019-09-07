452236e029a3c3a351a896a3de7b50d2081677dc
Sean <sean@roadturtlegames.com>
2019-09-06 23:10:15 -0400

    Initial project concept

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var playbookJS = [];
+playbookJS[0] = { name: "test", hosts: "all", become: true, tasks: [] };
+playbookJS[0].tasks[0] = {
+  name: "test template task",
+  template: { dest: "/etc/hosts", src: "hosts.j2" }
+};
+playbookJS[0].tasks[1] = {
+  name: "test ping task",
+  ping: null
+};
+
+console.log(jsyaml.dump(playbookJS));
